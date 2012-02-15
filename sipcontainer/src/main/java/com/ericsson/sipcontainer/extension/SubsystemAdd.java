8fb51232776ae6f768cbb469a5ad36ce65cce1f4
Miguel Reyes <mreyesab@redhat.com>
2012-02-15 16:41:24 +0200

    SIPContainer with the new SPI v0.1

--- sipcontainer/src/main/java/com/ericsson/sipcontainer/extension/SubsystemAdd.java
+++ sipcontainer/src/main/java/com/ericsson/sipcontainer/extension/SubsystemAdd.java
@@ -1,5 +1,6 @@
 package com.ericsson.sipcontainer.extension;
 
+import com.ericsson.sipcontainer.deployment.SipContextFactoryDeploymentProcessor;
 import java.util.List;
 
 import org.jboss.as.controller.AbstractBoottimeAddStepHandler;
@@ -12,7 +13,8 @@ import org.jboss.dmr.ModelNode;
 import org.jboss.logging.Logger;
 import org.jboss.msc.service.ServiceController;
 
-import com.ericsson.sipcontainer.deployment.SubsystemDeploymentProcessor;
+import com.ericsson.sipcontainer.deployment.SipMetaDataDeploymentProcessor;
+import org.jboss.as.server.deployment.Phase;
 
 /**
  * Handler responsible for adding the subsystem resource to the model
@@ -22,6 +24,8 @@ import com.ericsson.sipcontainer.deployment.SubsystemDeploymentProcessor;
 class SubsystemAdd extends AbstractBoottimeAddStepHandler {
 
     static final SubsystemAdd INSTANCE = new SubsystemAdd();
+    
+    static int DEPLOYMENT_PROCESS_PRIORITY = 0x4000;
 
     private final Logger log = Logger.getLogger(SubsystemAdd.class);
 
@@ -46,7 +50,11 @@ class SubsystemAdd extends AbstractBoottimeAddStepHandler {
         //see SubDeploymentProcessor for explanation of the phases
         context.addStep(new AbstractDeploymentChainStep() {
             public void execute(DeploymentProcessorTarget processorTarget) {
-                processorTarget.addDeploymentProcessor(SubsystemDeploymentProcessor.PHASE, SubsystemDeploymentProcessor.PRIORITY, new SubsystemDeploymentProcessor());
+                //processorTarget.addDeploymentProcessor(SubsystemDeploymentProcessor.PHASE, SubsystemDeploymentProcessor.PRIORITY, new SubsystemDeploymentProcessor());
+                // Add the SIP specific deployment processor
+                processorTarget.addDeploymentProcessor(Phase.PARSE, DEPLOYMENT_PROCESS_PRIORITY, SipMetaDataDeploymentProcessor.INSTANCE);
+                // Add the context in a different phase
+                processorTarget.addDeploymentProcessor(Phase.POST_MODULE, DEPLOYMENT_PROCESS_PRIORITY, SipContextFactoryDeploymentProcessor.INSTANCE);
 
             }
         }, OperationContext.Stage.RUNTIME);
