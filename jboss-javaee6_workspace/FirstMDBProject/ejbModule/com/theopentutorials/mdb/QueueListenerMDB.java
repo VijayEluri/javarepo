9c3aac71f0444464935ba7a6280ea128136fe71d
Miguel Reyes <mreyesab@redhat.com>
2012-05-04 16:26:52 +0200

    Followed tutorial.

--- jboss-javaee6_workspace/FirstMDBProject/ejbModule/com/theopentutorials/mdb/QueueListenerMDB.java
+++ jboss-javaee6_workspace/FirstMDBProject/ejbModule/com/theopentutorials/mdb/QueueListenerMDB.java
@@ -1,33 +1,43 @@
 package com.theopentutorials.mdb;
 
+import java.util.Date;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import javax.jms.TextMessage;
+import com.theopentutorials.mdb.to.Employee;
 
-/**
- * Message-Driven Bean implementation class for: QueueListenerMDB
- *
- */
-@MessageDriven(
-		activationConfig = { @ActivationConfigProperty(
-				propertyName = "destinationType", propertyValue = "javax.jms.Queue"
-		) })
+@MessageDriven(activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/MyQueue") })
 public class QueueListenerMDB implements MessageListener {
+	public QueueListenerMDB() {
+	}
 
-    /**
-     * Default constructor. 
-     */
-    public QueueListenerMDB() {
-        // TODO Auto-generated constructor stub
-    }
-	
-	/**
-     * @see MessageListener#onMessage(Message)
-     */
-    public void onMessage(Message message) {
-        // TODO Auto-generated method stub
-        
-    }
+	public void onMessage(Message message) {
+		try {
+			if (message instanceof TextMessage) {
+				System.out.println("Queue: I received a TextMessage at " + new Date());
+				TextMessage msg = (TextMessage) message;
+				System.out.println("Message is : " + msg.getText());
+			} else if (message instanceof ObjectMessage) {
+				System.out.println("Queue: I received an ObjectMessage at " + new Date());
+				ObjectMessage msg = (ObjectMessage) message;
+				Employee employee = (Employee) msg.getObject();
+				System.out.println("Employee Details: ");
+				System.out.println(employee.getId());
+				System.out.println(employee.getName());
+				System.out.println(employee.getDesignation());
+				System.out.println(employee.getSalary());
+			} else {
+				System.out.println("Not a valid message for this Queue MDB");
+			}
 
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
 }
