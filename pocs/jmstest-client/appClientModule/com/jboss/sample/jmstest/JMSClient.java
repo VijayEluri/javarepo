8562306e7e6b05d90f1371c48d4dd510292dcfca
Miguel Reyes <mreyesab@redhat.com>
2012-05-04 16:26:39 +0200

    Format

--- pocs/jmstest-client/appClientModule/com/jboss/sample/jmstest/JMSClient.java
+++ pocs/jmstest-client/appClientModule/com/jboss/sample/jmstest/JMSClient.java
@@ -14,69 +14,68 @@ import javax.naming.NamingException;
 
 import org.apache.log4j.Logger;
 
-
 public class JMSClient {
-    private static final Logger log = Logger.getLogger(JMSClient.class);
+	private static final Logger log = Logger.getLogger(JMSClient.class);
 
-    private final static String CF = "/ConnectionFactory";
-    private final static String DESTINATION = "queue/sample";
-    private final static String PROVIDER_URL = "jnp://localhost:1099";
+	private final static String CF = "/ConnectionFactory";
+	private final static String DESTINATION = "queue/sample";
+	private final static String PROVIDER_URL = "jnp://localhost:1099";
 
-    private static InitialContext context;
-    private static ConnectionFactory connectionFactory;
-    private static Connection conn;
-    private static Session session;
-    private static Destination destination;
-    private static MessageProducer producer;
+	private static InitialContext context;
+	private static ConnectionFactory connectionFactory;
+	private static Connection conn;
+	private static Session session;
+	private static Destination destination;
+	private static MessageProducer producer;
 
-    public static void main(String[] args) throws Exception {
-    run(100, 1000);
-    }
+	public static void main(String[] args) throws Exception {
+		run(100, 1000);
+	}
 
-    private static void run(int count, long delay) throws NamingException, Exception {
-    Properties jboss = new Properties();
-    jboss.put("java.naming.factory.initial", "org.jnp.interfaces.NamingContextFactory");
-    jboss.put("java.naming.factory.url.pkgs", "org.jboss.naming:org.jnp.interfaces");
-    jboss.put("java.naming.provider.url", PROVIDER_URL);
-    try {
-        context = new InitialContext(jboss);
-        connectionFactory = (ConnectionFactory) context.lookup(CF);
-        destination = (Destination)context.lookup(DESTINATION);
-        conn = connectionFactory.createConnection();
-        session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        producer = session.createProducer(destination);
-        conn.start();
+	private static void run(int count, long delay) throws NamingException, Exception {
+		Properties jboss = new Properties();
+		jboss.put("java.naming.factory.initial", "org.jnp.interfaces.NamingContextFactory");
+		jboss.put("java.naming.factory.url.pkgs", "org.jboss.naming:org.jnp.interfaces");
+		jboss.put("java.naming.provider.url", PROVIDER_URL);
+		try {
+			context = new InitialContext(jboss);
+			connectionFactory = (ConnectionFactory) context.lookup(CF);
+			destination = (Destination) context.lookup(DESTINATION);
+			conn = connectionFactory.createConnection();
+			session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			producer = session.createProducer(destination);
+			conn.start();
 
-        for (int i = 0; i < count; i++) {
-            send("Hello World " + i);
-            try {
-            Thread.sleep(delay);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        }
-    } catch (Exception e) {
-        e.printStackTrace();
-    } finally {
-        if (context != null) {
-            try {
-                context.close();
-            } catch (Exception ex) {
-            }
-        }
+			for (int i = 0; i < count; i++) {
+				send("Hello World " + i);
+				try {
+					Thread.sleep(delay);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (context != null) {
+				try {
+					context.close();
+				} catch (Exception ex) {
+				}
+			}
 
-        if (conn != null) {
-            try {
-                conn.close();
-            } catch (Exception e) {
-            }
-        }
-    }
-    }
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (Exception e) {
+				}
+			}
+		}
+	}
 
-    private static void send(String msg) throws JMSException {
-    TextMessage message = session.createTextMessage(msg);
-    producer.send(message);
-    log.info("Message sent: " + msg);
-    }
+	private static void send(String msg) throws JMSException {
+		TextMessage message = session.createTextMessage(msg);
+		producer.send(message);
+		log.info("Message sent: " + msg);
+	}
 }
